02f36b98afd555739bb9a0d3a977784db5e1f9ad
Baron <prefectdinorah@gmail.com>
2025-12-06 14:00:25 +0300

    Dark, light themes fot now. Waiting for previews

--- app/src/main/java/com/example/training/ui/theme/Theme.kt
+++ app/src/main/java/com/example/training/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.training.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,25 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFFEBFF57), // Lime yellow accent
+    secondary = Color(0xFF1C1C1E), // Card background
+    onPrimary = Color(0xFF2A2A2D), // Card text
+    background = Color(0xFF0A0A0A), // Very dark background
+    surface = Color(0xFF3A3A3C), // Light frame
+    surfaceVariant = Color(0xFF343434), // Dark frame
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Color(0xFFEBFF57), // Lime yellow accent
+    secondary = Color(0xFFFFFFFF), // Card background
+    onPrimary = Color(0xFF2A2A2D), // Card text
+    background = Color(0xFFFAFAFA), // Light gray background
+    surface = Color(0xFFFFFFFF), // Light frame
+    surfaceVariant = Color(0xFFFFFFFF) // Dark frame
 )
 
 @Composable
