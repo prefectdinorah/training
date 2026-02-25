0c62f2d95f2a6b7c04ea69c4b6a246c89b997dc9
Nikitosik73 <paramonov20@bk.ru>
2026-02-26 02:17:00 +0300

    Поправил navigation item для bottombar

--- feature/main/impl/src/main/java/feature/main/impl/navigation/NavigationItem.kt
+++ feature/main/impl/src/main/java/feature/main/impl/navigation/NavigationItem.kt
@@ -1,4 +1,4 @@
-package com.example.training.navigation
+package feature.main.impl.navigation
 
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.Icons.Outlined
@@ -12,32 +12,32 @@ import androidx.compose.material.icons.outlined.Restaurant
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation3.runtime.NavKey
-import com.example.training.R
+import feature.main.impl.R
 import fitness.component.bottom_nav.BottomNavigationItem
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface NavigationItem {
+sealed interface NavigationItem : NavKey, BottomNavigationItem {
     @Serializable
-    data object DashboardItem : NavigationItem, NavKey, BottomNavigationItem {
-        override val title: Int = R.string.navigation_item_dashboard
-        override val selectedIcon: ImageVector = Filled.Restaurant
-        override val unselectedIcon: ImageVector = Outlined.Restaurant
-    }
-    @Serializable
-    data object WorkoutItem : NavigationItem, NavKey, BottomNavigationItem {
+    data object WorkoutItem : NavigationItem {
         override val title: Int = R.string.navigation_item_workout
         override val selectedIcon: ImageVector = Filled.FitnessCenter
         override val unselectedIcon: ImageVector = Outlined.FitnessCenter
     }
     @Serializable
-    data object ProfileItem : NavigationItem, NavKey, BottomNavigationItem {
-        override val title: Int = R.string.navigation_item_profile
+    data object FoodItem : NavigationItem {
+        override val title: Int = R.string.navigation_item_food
+        override val selectedIcon: ImageVector = Filled.Restaurant
+        override val unselectedIcon: ImageVector = Outlined.Restaurant
+    }
+    @Serializable
+    data object TrainersItem : NavigationItem {
+        override val title: Int = R.string.navigation_item_trainers
         override val selectedIcon: ImageVector = Filled.Person
         override val unselectedIcon: ImageVector = Outlined.Person
     }
     @Serializable
-    data object SettingsItem : NavigationItem, NavKey, BottomNavigationItem {
+    data object SettingsItem : NavigationItem {
         override val title: Int = R.string.navigation_item_settings
         override val selectedIcon: ImageVector = Filled.Settings
         override val unselectedIcon: ImageVector = Outlined.Settings
